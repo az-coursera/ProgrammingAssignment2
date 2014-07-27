e71ebdc2d292d52c6d26704b99d1bdcdced63bc9
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-07-28 02:06:40 +0400

    solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I don't know if I should write any description for that 'cause it's just obvious.
+## Anyway it's just copy-paste of the example from programming assignment page,
+## that adapted to create a special object (with the matrix inside) 
+## that is able to cache the result of solve() function. 
 
-## Write a short comment describing this function
-
+## creates special object with cache
 makeCacheMatrix <- function(x = matrix()) {
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
 
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## caching function that returns an inverted matix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
